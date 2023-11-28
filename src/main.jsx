5414643116f55945ae63f8c90e592e3b65c0eba3
Yemny <y.feliz1296@gmail.com>
2023-11-28 13:21:19 -0500

    imported components and modules to crate routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,46 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}>
     <App />
-  </React.StrictMode>,
-)
+    </RouterProvider>
+  </React.StrictMode>
+);
