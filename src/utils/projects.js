02c7d6d600120c0c7e65c787ee8b53a9227e2a9c
Yemny <y.feliz1296@gmail.com>
2023-12-02 19:21:27 -0500

    added more images

---
+++ src/utils/projects.js
@@ -0,0 +1,6 @@
+import passwordGenerator from '../assets/passwordGenerator.png';
+import timedQuizz from '../assets/timedQuizz.png';
+import timedQuizz from '../assets/timedQuizz.png';
+import timedQuizz from '../assets/timedQuizz.png';
+import timedQuizz from '../assets/timedQuizz.png';
+import timedQuizz from '../assets/timedQuizz.png';
